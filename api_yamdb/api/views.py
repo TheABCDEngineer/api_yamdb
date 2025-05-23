1749ad0fc8293ead0f6876cfd9474f8ee8704b65
Anton Komyagin <107632390+TheABCDEngineer@users.noreply.github.com>
2025-05-23 14:00:35 +0300

    Update views.py

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,24 +1,39 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
-from django.db.models import Avg, Prefetch
+from django.db.models import Avg
 from django.shortcuts import get_object_or_404
 from rest_framework import filters, status, viewsets
 from rest_framework.decorators import action
-from rest_framework.permissions import (AllowAny, IsAuthenticated,
-                                        IsAuthenticatedOrReadOnly)
+from rest_framework.permissions import (
+    AllowAny,
+    IsAuthenticated,
+    IsAuthenticatedOrReadOnly
+)
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import AccessToken
 
 from reviews.models import Category, Genre, Review, Title
+
 from .filters import TitleFilter
-from .permissions import AdminOnly, IsAdminOrReadOnly, IsAuthorOrReadOnly
-from .serializers import (CategorySerializer, CommentSerializer,
-                          GenreSerializer, ReviewSerializer,
-                          TitleGetSerializer, TitlePostSerializer,
-                          TokenSerializer, UserMeSerializer,
-                          UsernameEmailSreializer, UserSerializer)
+from .permissions import (
+    AdminOnly,
+    IsAdminOrReadOnly,
+    IsAuthorOrReadOnly
+)
+from .serializers import (
+    CategorySerializer,
+    CommentSerializer,
+    GenreSerializer,
+    ReviewSerializer,
+    TitleGetSerializer,
+    TitlePostSerializer,
+    TokenSerializer,
+    UserMeSerializer,
+    UsernameEmailSreializer,
+    UserSerializer
+)
 
 
 User = get_user_model()
@@ -84,7 +99,7 @@ class ReviewViewSet(viewsets.ModelViewSet):
         )
 
 
-class TitleViewSet(viewsets.ModelViewSet): 
+class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.annotate(
         rating=Avg('reviews__score')
     ).select_related('category').prefetch_related('genre')
